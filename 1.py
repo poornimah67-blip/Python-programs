47f2a532ea732a79b97e3d031d0dab2e7edeb444
poornimah67-blip <poornimah67@gmail.com>
2026-03-16 18:17:28 +0530

    Add hello world and sum program in 1.py

---
+++ 1.py
@@ -0,0 +1,13 @@
+# program to print hello world 
+print("Hello, World!")
+
+
+
+# Program to add two numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = a + b
+
+print("Sum =", sum)
