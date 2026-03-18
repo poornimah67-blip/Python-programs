fe77365f36d08082ca14ffd33ae0c7969d53c448
poornimah67-blip <poornimah67@gmail.com>
2026-03-18 23:11:31 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,6 +1,20 @@
+# pgm to subtract two numbers 
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
-
 result = a - b
-
 print("Subtraction =", result)
+
+# pgm to multiply two numbers 
+a= int(input(" enter a:"))
+b= int(input(" enter b:"))
+product=a*b
+print("product is=", product)
+
+#pgm to div two int numbers
+a= int(input(" enter a:"))
+b= int(input(" enter b:"))
+div=a/b
+print(" the final value is:", div)
+             
+
+
